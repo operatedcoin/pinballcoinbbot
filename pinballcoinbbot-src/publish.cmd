2ac13c806d3cbee1267f7e8f6601586a70a1c4bb
operatedcoin <62930557+operatedcoin@users.noreply.github.com>
2021-01-26 18:36:43 +1100

    Add files via upload

---
+++ pinballcoinbbot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild EchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=.\pinballcoinbbot-Web-Deploy.pubxml -p:Password=iDwkPfb5XblMi1TLcjpaw2MtSicLuGxLadbno4PbJaSATQz3M87pW1JGtGAu
+
